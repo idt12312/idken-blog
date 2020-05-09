4b744000bdcb7857b01e5a98f7defc45ddbd7d4f
idt12312 <idt12312@gmail.com>
2020-05-09 12:32:27 +0900

    Docker composeを使ってJekyllを動かす設定をした

--- build.ps1
+++
@@ -1,1 +0,0 @@
-docker run --rm --name=jekyll --volume="$PWD":/srv/jekyll --volume="$PWD"/vendor/bundle:/usr/local/bundle -it jekyll/jekyll jekyll build --incremental
--- serve.ps1
+++
@@ -1,1 +0,0 @@
-docker run --rm --name=jekyll --volume="$PWD":/srv/jekyll --volume="$PWD"/vendor/bundle:/usr/local/bundle -it -p 4000:4000 jekyll/jekyll jekyll serve --incremental
